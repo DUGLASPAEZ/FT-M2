b3111f621c2c3d37b92a98e650f1e02e767ad15f
DUGLASPAEZ <paez18730@gmail.com>
2023-03-29 15:02:00 -0600

    react intro app

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-
-const studentName = "";
+import Botones from "./Botones"
+const studentName = "Duglas Paez";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1> primera app con react yipiiiii</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {
+          techSkills.map((skill, index) => <li key = {index}>{skill}</li>)
+            
+          }
+        
+      </ul>
+      <Botones alerts={alerts}/>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
